191ba0d0fb30acbc7fa0a921efe43eb689086647
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-23 12:23:25 +0900

    Installed composer

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ikumo1118/backend',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ab3ec634e17793e7048a5bee9f49e9e95f6c5a8a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ikumo1118/backend' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ab3ec634e17793e7048a5bee9f49e9e95f6c5a8a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
