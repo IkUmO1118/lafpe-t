e32a53dcac6a4162b28cb47c04399730590e48e8
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-24 22:19:54 +0900

    Implemented frontend base

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="py-2 text-red-600 sm:px-8">Hello world</h1>;
+  return <h1 className="text-xl font-bold text-red-500">Hello World</h1>;
 }
 
 export default App;
