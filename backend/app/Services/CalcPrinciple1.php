463772d063fd14a3ff06fe17ff5548e677ee9002
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-28 07:48:32 +0900

    Implemented Pinrciple1

--- backend/app/Services/CalcPrinciple1.php
+++ backend/app/Services/CalcPrinciple1.php
@@ -19,15 +19,29 @@ class CalcPrinciple1 extends AbstractPrinciple
 
   private function calcQ3(): void
   {
-    $resQ3 = $this->res["Q3"];
+    $resQ3 = $this->res['Q3'];
+
+    if (isset($resQ3) && !empty($resQ3)) {
+      $totalStaticPoint = $this->staticPoints['Q3'] ?? 0;
+      $this->addTotalScore($totalStaticPoint);
+
+      // 各項目のisCheckedを確認し、trueの場合は対応するポイントを加算
+      foreach ($resQ3 as $key => $value) {
+        if (isset($value['isChecked']) && $value['isChecked']) {
+          $times = $value['times'] ?? 0;
+          $dynamicPoint = $this->dynamicPoints['Q3'][$key][$times] ?? 0;
+          $this->addTotalScore($dynamicPoint);
+        }
+      }
+    }
   }
 
   private function calcQ13(): void
   {
-    $resQ13 = $this->res["Q13"];
+    $resQ13 = $this->res['Q13'];
 
     if (isset($resQ13) && !empty($resQ13)) {
-      $totalStaticPoint = $this->staticPoints['Q13'];
+      $totalStaticPoint = $this->staticPoints['Q13'] ?? 0;
 
       $totalWeighting = array_reduce(
         $resQ13,
