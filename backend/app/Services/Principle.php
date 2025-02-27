3eb1cc9fbb57a870f2f9eb7b9c3c246b560f8408
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-27 09:38:03 +0900

    Added getTotalScore method to interface

--- backend/app/Services/Principle.php
+++ backend/app/Services/Principle.php
@@ -5,5 +5,6 @@ namespace app\Services;
 interface Principle
 {
   public static function getPrinciple(): string;
+  public function getTotalScore(): float;
   public function calculate(array $res): float;
 }
