a8fec3be655d11e744bf840ad2b3d04e4c6e64fc
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-02-28 09:59:46 +0900

    Implemented Principle 2

--- backend/app/Services/CalcPrinciple2.php
+++ backend/app/Services/CalcPrinciple2.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace app\Services;
+
 use app\Services\Contracts\AbstractPrinciple;
 
 class CalcPrinciple2 extends AbstractPrinciple
@@ -19,32 +21,131 @@ class CalcPrinciple2 extends AbstractPrinciple
 
     return $this->getTotalScore();
   }
+
   private function calcQ1(): void
   {
     $resQ1 = $this->res['Q1'];
+
+    if (isset($resQ1) && !empty($resQ1)) {
+      $totalStaticScore = $this->staticPoints["Q1"] ?? 0;
+
+      $totalWeighting = array_reduce(
+        $resQ1,
+        function ($acc, $cur) {
+          return $acc + ($this->weightings["Q1"][$cur] ?? 0);
+        },
+        0
+      );
+
+      $this->addTotalScore($totalStaticScore * $totalWeighting);
+      echo 'Q1: ' . $totalStaticScore * $totalWeighting . PHP_EOL;
+    }
   }
+
   private function calcQ2(): void
   {
-    $resQ1 = $this->res['Q2'];
+    $resQ2 = $this->res['Q2'];
+
+    if (isset($resQ2) && !empty($resQ2)) {
+      $totalStaticScore = $this->staticPoints["Q2"] ?? 0;
+
+      $totalWeighting = array_reduce(
+        $resQ2,
+        function ($acc, $cur) {
+          return $acc + ($this->weightings["Q2"][$cur] ?? 0);
+        },
+        0
+      );
+
+      $this->addTotalScore($totalStaticScore * $totalWeighting);
+      echo 'Q2: ' . $totalStaticScore * $totalWeighting . PHP_EOL;
+    }
   }
+
   private function calcQ3(): void
   {
-    $resQ1 = $this->res['Q3'];
+    $resQ3 = $this->res['Q3'];
+
+    if (isset($resQ3) && !empty($resQ3)) {
+      $totalStaticPoint = $this->staticPoints['Q3'] ?? 0;
+      $totalWeighting = 0;
+
+      foreach ($resQ3 as $rackKey => $rackValue) {
+        if (isset($rackValue['isChecked']) && $rackValue['isChecked']) {
+
+          $rackWeight  = $this->weightings['Q3'][$rackKey]['rackWeighting'] ?? 0;
+          $perWeight   = $this->weightings['Q3'][$rackKey]['perWeighting'][$rackValue['per']] ?? 0;
+          $timesWeight = $this->weightings['Q3'][$rackKey]['timesWeighting'][$rackValue['times']] ?? 0;
+
+          $totalWeighting += $rackWeight * $perWeight * $timesWeight;
+        }
+      }
+
+      $this->addTotalScore($totalWeighting * $totalStaticPoint);
+    }
+
+    echo 'Q3: ' . $totalWeighting * $this->staticPoints['Q3'] . PHP_EOL;
   }
+
   private function calcQ5(): void
   {
-    $resQ1 = $this->res['Q5'];
+    $resQ5 = $this->res['Q5'];
+
+    if ($resQ5 === 0 || $resQ5) {
+      $this->addTotalScore($this->staticPoints["Q5"] * $this->weightings["Q5"][$resQ5]);
+    }
+
+    echo 'Q5: ' . $this->staticPoints["Q5"] * $this->weightings["Q5"][$resQ5] . PHP_EOL;
   }
+
   private function calcQ6(): void
   {
-    $resQ1 = $this->res['Q6'];
+    $resQ6 = $this->res['Q6'];
+
+    if ($resQ6 === 0 || $resQ6) {
+      $this->addTotalScore($this->staticPoints["Q6"] * $this->weightings["Q6"][$resQ6]);
+    }
+
+    echo 'Q6: ' . $this->staticPoints["Q6"] * $this->weightings["Q6"][$resQ6] . PHP_EOL;
   }
+
   private function calcQ7(): void
   {
-    $resQ1 = $this->res['Q7'];
+    $resQ7 = $this->res['Q7'];
+
+    if (isset($resQ7) && !empty($resQ7)) {
+      $totalStaticScore = $this->staticPoints["Q7"] ?? 0;
+
+      $totalWeighting = array_reduce(
+        $resQ7,
+        function ($acc, $cur) {
+          return $acc + ($this->weightings["Q7"][$cur] ?? 0);
+        },
+        0
+      );
+
+      $this->addTotalScore($totalStaticScore * $totalWeighting);
+      echo 'Q7: ' . $totalStaticScore * $totalWeighting . PHP_EOL;
+    }
   }
+
   private function calcQ8(): void
   {
-    $resQ1 = $this->res['Q8'];
+    $resQ8 = $this->res['Q8'];
+
+    if (isset($resQ8) && !empty($resQ8)) {
+      $totalStaticScore = $this->staticPoints["Q8"] ?? 0;
+
+      $totalWeighting = array_reduce(
+        $resQ8,
+        function ($acc, $cur) {
+          return $acc + ($this->weightings["Q8"][$cur] ?? 0);
+        },
+        0
+      );
+
+      $this->addTotalScore($totalStaticScore * $totalWeighting);
+      echo 'Q8: ' . $totalStaticScore * $totalWeighting . PHP_EOL;
+    }
   }
 }
