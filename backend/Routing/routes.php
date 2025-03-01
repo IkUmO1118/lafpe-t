6275dc22d9ac5c19cf5262fa6440016cde784075
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-02 08:11:33 +0900

    Added POST api/diagnosis

--- backend/Routing/routes.php
+++ backend/Routing/routes.php
@@ -1,6 +1,30 @@
 <?php
 
+namespace Routing;
+
+use app\Controllers\DiagnosisController;
+use Helpers\ValidationHelper;
+use Response\HTTPRenderer;
+use Response\Render\JSONRenderer;
+
 return [
-  "POST /diagnoses" => "calculate",
-  "PUT /diagnoses" => "recalculate"
+  "api/diagnosis" => [
+    'POST' => function (): HTTPRenderer {
+      $requestData = json_decode(file_get_contents('php://input'), true);
+      $data = ValidationHelper::checkRequiredQuestions($requestData);
+
+      $controller = new DiagnosisController($data);
+      $diagnosis = $controller->store();
+      return new JSONRenderer(['diagnosis' => $diagnosis]);
+    },
+
+    'PUT' => function (): HTTPRenderer {
+      $requestData = json_decode(file_get_contents('php://input'), true);
+      $data = ValidationHelper::checkRequiredQuestions($requestData);
+
+      $controller = new DiagnosisController($data);
+      $diagnosis = $controller->update();
+      return new JSONRenderer(['diagnosis' => $diagnosis]);
+    }
+  ]
 ];
