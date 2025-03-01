af63ebcc1ac5945eab0103080058f74251ec9ef2
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-01 09:03:30 +0900

    Added principle 3 into controller

--- backend/app/Controllers/DiagnosisController.php
+++ backend/app/Controllers/DiagnosisController.php
@@ -4,6 +4,7 @@ namespace app\Controllers;
 
 use app\Services\CalcPrinciple1;
 use app\Services\CalcPrinciple2;
+use app\Services\CalcPrinciple3;
 use Helpers\ValidationHelper;
 use Response\HTTPRenderer;
 use Response\Render\JSONRenderer;
@@ -12,7 +13,7 @@ class DiagnosisController
 {
   private const PRINCIPLE1_QUESTIONS = ['Q3', 'Q13'];
   private const PRINCIPLE2_QUESTIONS = ['Q1', 'Q2', 'Q3', 'Q5', 'Q6', 'Q7', 'Q8'];
-  // private const PRINCIPLE3_QUESTIONS = ["Q3", "Q4", 'Q5', 'Q6', 'Q7', 'Q8', "Q9"];
+  private const PRINCIPLE3_QUESTIONS = ["Q3", "Q4", 'Q5', 'Q6', 'Q7', 'Q8', "Q9"];
   // private const PRINCIPLE4_QUESTIONS = ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q10'];
   // private const PRINCIPLE5_QUESTIONS = ["Q3", "Q12"];
   // private const PRINCIPLEDP_QUESTIONS = ["Q4", "Q11"];
@@ -76,13 +77,16 @@ class DiagnosisController
       // 各原則用のデータを抽出してからインスタンス化
       $principle1Data = array_intersect_key($requestData, array_flip(self::PRINCIPLE1_QUESTIONS));
       $principle2Data = array_intersect_key($requestData, array_flip(self::PRINCIPLE2_QUESTIONS));
+      $principle3Data = array_intersect_key($requestData, array_flip(self::PRINCIPLE3_QUESTIONS));
 
       $principle1 = new CalcPrinciple1($principle1Data);
       $principle2 = new CalcPrinciple2($principle2Data);
+      $principle3 = new CalcPrinciple3($principle3Data);
 
       $result = [
         'principle1' => $principle1->calculate(),
-        'principle2' => $principle2->calculate()
+        'principle2' => $principle2->calculate(),
+        'principle3' => $principle3->calculate()
       ];
 
       echo "\nCalculation results:\n";
