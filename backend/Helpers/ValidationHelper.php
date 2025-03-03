b92b9e2a01a50b79ce43e3f9063f95974501fd6f
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-03 14:18:33 +0900

    Changed return value

--- backend/Helpers/ValidationHelper.php
+++ backend/Helpers/ValidationHelper.php
@@ -7,7 +7,7 @@ class ValidationHelper
   public static function checkRequiredQuestions(array $data): array
   {
     if (!is_array($data)) {
-      return false;
+      throw new \InvalidArgumentException('The provided value is not a valid data.');
     }
 
     $requiredQuestions = ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13'];
