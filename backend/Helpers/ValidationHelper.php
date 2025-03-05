dad86aa217d9dcaf6775527203a3263ddebc3991
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-05 14:04:03 +0900

    Relaxed argument type restrictions

--- backend/Helpers/ValidationHelper.php
+++ backend/Helpers/ValidationHelper.php
@@ -2,19 +2,21 @@
 
 namespace Helpers;
 
+use InvalidArgumentException;
+
 class ValidationHelper
 {
-  public static function checkRequiredQuestions(array $data): array
+  public static function checkRequiredQuestions($data): array
   {
     if (!is_array($data)) {
-      throw new \InvalidArgumentException('The provided value is not a valid data.');
+      throw new InvalidArgumentException('The provided value is not a valid data.');
     }
 
     $requiredQuestions = ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13'];
 
     foreach ($requiredQuestions as $question) {
       if (!isset($data[$question])) {
-        throw new \InvalidArgumentException('The provided value is not a valid data.');
+        throw new InvalidArgumentException('The provided value is not a valid data.');
       }
     }
 
