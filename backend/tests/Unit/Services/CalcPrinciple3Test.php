56195aa3a15479df400b59f25bec98de57ab8a60
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-08 11:48:55 +0900

    Added full_data testFn for principle 3

--- backend/tests/Unit/Services/CalcPrinciple3Test.php
+++ backend/tests/Unit/Services/CalcPrinciple3Test.php
@@ -1,9 +1,34 @@
 <?php
 
+use App\Services\CalcPrinciple3;
 use PHPUnit\Framework\TestCase;
 
 class CalcPrinciple3Test extends TestCase
 {
+  public function testCalculate_FullData()
+  {
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => 0,
+      'Q6' => 2,
+      'Q7' => [1],
+      'Q8' => [0],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $score = $principle3->calculate();
+
+    $this->assertEquals(5.76, $score);
+  }
   /**
    * empty data
    */
