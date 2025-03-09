f44e673397f32620add514d95d28a1ddd97fa378
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-09 15:39:22 +0900

    Added null_element testFn for principle 3

--- backend/tests/Unit/Services/CalcPrinciple3Test.php
+++ backend/tests/Unit/Services/CalcPrinciple3Test.php
@@ -494,15 +494,190 @@ class CalcPrinciple3Test extends TestCase
   /**
    * check null
    */
-  public function testQ2_NullElement() {}
-  public function testQ3_NullPerParameter() {}
-  public function testQ3_NullTimesParameter() {}
-  public function testQ4_NullElement() {}
-  public function testQ5_NullElement() {}
-  public function testQ6_NullElement() {}
-  public function testQ7_NullElement() {}
-  public function testQ8_NullElement() {}
-  public function testQ9_NullElement() {}
+  public function testQ3_NullPerParameter()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => null, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => 0,
+      'Q6' => 2,
+      'Q7' => [1],
+      'Q8' => [0],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
+  public function testQ3_NullTimesParameter()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => null],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => 0,
+      'Q6' => 2,
+      'Q7' => [1],
+      'Q8' => [0],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
+  public function testQ4_NullElement()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => null,
+      'Q5' => 0,
+      'Q6' => 2,
+      'Q7' => [1],
+      'Q8' => [0],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
+  public function testQ5_NullElement()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => null,
+      'Q6' => 2,
+      'Q7' => [1],
+      'Q8' => [0],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
+  public function testQ6_NullElement()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => 0,
+      'Q6' => null,
+      'Q7' => [1],
+      'Q8' => [0],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
+  public function testQ7_NullElement()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => 0,
+      'Q6' => 2,
+      'Q7' => [null],
+      'Q8' => [0],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
+  public function testQ8_NullElement()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => 0,
+      'Q6' => 2,
+      'Q7' => [1],
+      'Q8' => [null],
+      'Q9' => [0, 1],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
+  public function testQ9_NullElement()
+  {
+    $this->expectException(\Exception::class);
+    $data = [
+      'Q3' => [
+        'openRack' => ['isChecked' => false],
+        'IVCRack' => ['isChecked' => false],
+        'positiveRack' => ['isChecked' => true, 'per' => 0, 'times' => 3],
+        'negativeRack' => ['isChecked' => false],
+        'oneWayAirflowRack' => ['isChecked' => true, 'per' => 4, 'times' => 2],
+        'isolator' => ['isChecked' => false]
+      ],
+      'Q4' => 1,
+      'Q5' => 0,
+      'Q6' => 2,
+      'Q7' => [1],
+      'Q8' => [0],
+      'Q9' => [null],
+    ];
+
+    $principle3 = new CalcPrinciple3($data);
+    $principle3->calculate();
+  }
 
   /**
    * check boundary
