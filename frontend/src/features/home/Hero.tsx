3e3ee09a170dc1e2a90af79e71d3263e874068f3
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-12 17:07:28 +0900

    Added hero section

---
+++ frontend/src/features/home/Hero.tsx
@@ -0,0 +1,33 @@
+import Button from "../../components/Button";
+
+function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-10 pt-20 pb-16">
+      <h1 className="text-5xl font-bold text-cyan-950">
+        実験動物施設性能診断ツールで
+        <br />
+        施設の性能を評価し、最適化する
+      </h1>
+      <div className="flex gap-4">
+        <Button
+          as="button"
+          type="fill"
+          color="primary"
+          className="rounded-sm px-6 py-4 text-base font-bold"
+        >
+          診断を開始する
+        </Button>
+        <Button
+          as="button"
+          type="outline"
+          color="primary"
+          className="rounded-sm px-6 py-4 text-base font-medium"
+        >
+          ツールについて
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
