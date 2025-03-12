6cc5c481237955a9ad1770cc5bec51cce1683a6c
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-12 17:07:48 +0900

    Added ToolBanner section

---
+++ frontend/src/features/home/ToolBanner.tsx
@@ -0,0 +1,14 @@
+function ToolBanner() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 bg-slate-100 py-16 text-neutral-800">
+      <h3 className="text-[28px] font-medium">ツールを理解する</h3>
+      <p className="text-lg font-normal">
+        このツールにより、実験動物施設の性能を多面的・多角的に捉え、評価結果を視覚化し、施設の最適化を図ることができ
+        <br />
+        る。さらに既存施設の対象とするだけでなく、新築計画時及び改修時の参考や第三者による施設の査察として活用できる
+      </p>
+    </div>
+  );
+}
+
+export default ToolBanner;
