cc016ca683e752f0dfa8ff347759bb5eddd827d2
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-13 17:57:28 +0900

    Added try catch block

--- backend/app/Controllers/DiagnosisController.php
+++ backend/app/Controllers/DiagnosisController.php
@@ -27,33 +27,37 @@ class DiagnosisController
 
   public function store(): array
   {
-    // 各原則用のデータを抽出してからインスタンス化
-    $principle1Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE1_QUESTIONS));
-    $principle2Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE2_QUESTIONS));
-    $principle3Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE3_QUESTIONS));
-    $principle4Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE4_QUESTIONS));
-    $principle5Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE5_QUESTIONS));
-    $principleDPData = array_intersect_key($this->data, array_flip(self::PRINCIPLEDP_QUESTIONS));
+    try {
+      // 各原則用のデータを抽出してからインスタンス化
+      $principle1Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE1_QUESTIONS));
+      $principle2Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE2_QUESTIONS));
+      $principle3Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE3_QUESTIONS));
+      $principle4Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE4_QUESTIONS));
+      $principle5Data = array_intersect_key($this->data, array_flip(self::PRINCIPLE5_QUESTIONS));
+      $principleDPData = array_intersect_key($this->data, array_flip(self::PRINCIPLEDP_QUESTIONS));
 
-    $principle1 = new CalcPrinciple1($principle1Data);
-    $principle2 = new CalcPrinciple2($principle2Data);
-    $principle3 = new CalcPrinciple3($principle3Data);
-    $principle4 = new CalcPrinciple4($principle4Data);
-    $principle5 = new CalcPrinciple5($principle5Data);
-    $principleDP = new CalcPrincipleDP($principleDPData);
+      $principle1 = new CalcPrinciple1($principle1Data);
+      $principle2 = new CalcPrinciple2($principle2Data);
+      $principle3 = new CalcPrinciple3($principle3Data);
+      $principle4 = new CalcPrinciple4($principle4Data);
+      $principle5 = new CalcPrinciple5($principle5Data);
+      $principleDP = new CalcPrincipleDP($principleDPData);
 
-    $result = [
-      'principle1' => $principle1->calculate(),
-      'principle2' => $principle2->calculate(),
-      'principle3' => $principle3->calculate(),
-      'principle4' => $principle4->calculate(),
-      'principle5' => $principle5->calculate(),
-      'principleDP' => $principleDP->calculate(),
-    ];
+      $result = [
+        'principle1' => $principle1->calculate(),
+        'principle2' => $principle2->calculate(),
+        'principle3' => $principle3->calculate(),
+        'principle4' => $principle4->calculate(),
+        'principle5' => $principle5->calculate(),
+        'principleDP' => $principleDP->calculate(),
+      ];
 
-    if (!$result) throw new Exception('Failed to store diagnosis data');
+      if (!$result) throw new Exception('Failed to store diagnosis data');
 
-    return $result;
+      return $result;
+    } catch (Exception $e) {
+      throw new Exception('Error storing message: ' . $e->getMessage());
+    }
   }
 
   public function update(): array
