437d40158f7b7894bc619301565c823e9ba24119
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-15 08:55:27 +0900

    Added spinner component

---
+++ frontend/src/components/SpinnerMini.tsx
@@ -0,0 +1,7 @@
+function SpinnerMini() {
+  return (
+    <div className="h-6 w-6 animate-spin rounded-full border-2 border-solid border-current border-t-transparent"></div>
+  );
+}
+
+export default SpinnerMini;
