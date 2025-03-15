7aac88f98c91874a3d05f3e7676bbd741f0e9d4d
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-15 09:11:00 +0900

    Added toast, react-query and react-query-devtools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Navigate, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes } from "react-router";
+
 import Home from "./pages/Home";
 import Result from "./pages/Result";
 import PageNotFound from "./pages/PageNotFound";
@@ -8,22 +11,58 @@ import AppLayout from "./layouts/AppLayout";
 import Feedback from "./pages/Feedback";
 import ScrollToTop from "./components/ScrollToTop";
 
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="home" />} />
-          <Route path="home" element={<Home />} />
-          <Route path="feedback" element={<Feedback />} />
-          <Route path="result" element={<Result />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
 
-        <Route path="diagnosis" element={<Diagnosis />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="home" />} />
+            <Route path="home" element={<Home />} />
+            <Route path="feedback" element={<Feedback />} />
+            <Route path="result" element={<Result />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+
+          <Route path="diagnosis" element={<Diagnosis />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Toaster
+        position="top-right"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "#f5f5f5",
+            color: "#262626",
+          },
+        }}
+      />
+    </QueryClientProvider>
   );
 }
 
