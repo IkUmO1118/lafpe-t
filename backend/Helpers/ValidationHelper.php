71ae8e413ccd7aec71230ed935cdf2fe8aca0328
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-16 15:21:34 +0900

    Added question helper

--- backend/Helpers/ValidationHelper.php
+++ backend/Helpers/ValidationHelper.php
@@ -23,6 +23,23 @@ class ValidationHelper
     return $data;
   }
 
+  public static function checkRequiredPrinciples(array $data): array
+  {
+    if (!is_array($data)) {
+      throw new InvalidArgumentException('The provided value is not a valid data.');
+    }
+
+    $requiredPrinciples = ["principle1", "principle2", "principle3", "principle4", "principle5", "principleDP"];
+
+    foreach ($requiredPrinciples as $principle) {
+      if (!isset($data[$principle])) {
+        throw new InvalidArgumentException('The provided value is not a valid data.');
+      }
+    }
+
+    return $data;
+  }
+
   public static function string(array $value): array
   {
     if (!isset($value["message"]) || !is_string($value["message"])) {
