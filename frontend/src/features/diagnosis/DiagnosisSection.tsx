b0001451c69b97f7462c327bc769e3dfbb136c74
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-19 18:15:26 +0900

    Added Diagnosis section

---
+++ frontend/src/features/diagnosis/DiagnosisSection.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import DiagnosisHeader from "./DiagnosisHeader";
+import { Button } from "../../components/Button";
+
+function DiagnosisSection() {
+  const [index, setIndex] = useState<number>(0);
+
+  return (
+    <section className="h-full w-full rounded-xl bg-slate-50 px-8 py-7 shadow-lg">
+      <div className="flex flex-col gap-12">
+        <DiagnosisHeader index={index} setIndex={setIndex} />
+
+        <div className="flex w-1/2 flex-col gap-7 self-center">
+          <form className="flex flex-col gap-3">
+            <div className="self-center text-sm font-bold text-cyan-700">
+              設問&nbsp;{index + 1}
+            </div>
+            <p className="mb-2.5 border-b-2 border-dotted border-neutral-300 pb-2.5 text-base font-bold text-neutral-700">
+              施設の廊下方式を選択してください（複数選択可）
+            </p>
+          </form>
+          <div className="mb-5">form</div>
+          <Button
+            className="self-center py-3 font-medium"
+            onClick={() => {
+              if (index < 12) {
+                setIndex((i) => i + 1);
+              }
+            }}
+          >
+            次の設問へ&nbsp;&nbsp;&rarr;
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default DiagnosisSection;
