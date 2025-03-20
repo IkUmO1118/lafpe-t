f26eb7bc563d09fc425896fb2f19e9df4c6f75ac
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-20 12:42:43 +0900

    Chaged type

--- frontend/src/types/diagnosis.d.ts
+++ frontend/src/types/diagnosis.d.ts
@@ -19,22 +19,61 @@ export type NestedRadioScore = {
 export type QuestionKeys =
   | "Q1"
   | "Q2"
+  | "Q4"
+  | "Q5"
+  | "Q6"
+  | "Q3"
   | "Q7"
   | "Q8"
   | "Q9"
   | "Q10"
   | "Q11"
   | "Q12"
-  | "Q13"
-  | "Q4"
-  | "Q5"
-  | "Q6"
-  | "Q3";
+  | "Q13";
 
 export type ScoresState = {
-  [K in QuestionKeys]: K extends "Q3"
-    ? NestedRadioScore
-    : K extends "Q4" | "Q5" | "Q6"
-      ? RadioScore
-      : CheckboxScore;
+  Q1: number[];
+  Q2: number[];
+  Q3: {
+    オープンラック: {
+      isChecked: false;
+      per?: number;
+      times?: number;
+    };
+    IVCラック: {
+      isChecked: false;
+      per?: number;
+      times?: number;
+    };
+    陽圧ラック: {
+      isChecked: false;
+      per?: number;
+      times?: number;
+    };
+    陰圧ラック: {
+      isChecked: false;
+      per?: number;
+      times?: number;
+    };
+    一方向気流ラック: {
+      isChecked: false;
+      per?: number;
+      times?: number;
+    };
+    アイソレータ: {
+      isChecked: false;
+      per?: number;
+      times?: number;
+    };
+  };
+  Q4: number | null;
+  Q5: number | null;
+  Q6: number | null;
+  Q7: number[];
+  Q8: number[];
+  Q9: number[];
+  Q10: number[];
+  Q11: number[];
+  Q12: number[];
+  Q13: number[];
 };
