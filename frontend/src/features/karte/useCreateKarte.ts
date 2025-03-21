50baba7c3669e079b93046e45c81461d13bc1b02
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-21 14:47:59 +0900

    Just implemented createKarte using react-query

---
+++ frontend/src/features/karte/useCreateKarte.ts
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useSetSession as SetSession } from "../../hooks/useSession";
+import { postDiagnosis as postDiagnosisApi } from "../../services/apiDiagnosis";
+
+export function useCreateKarte() {
+  const navigate = useNavigate();
+
+  const { mutate: createKarte, isPending: isCreating } = useMutation({
+    mutationFn: postDiagnosisApi,
+    onSuccess: (data) => {
+      SetSession({
+        key: "diagnosis-result",
+        value: `${JSON.stringify(data.diagnosis)}`,
+      });
+      navigate("/result");
+    },
+    onError: (err) => console.error(err.message),
+  });
+
+  return { createKarte, isCreating };
+}
