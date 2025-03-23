0c6cf59d368ddc62b11a0029823ca6723671e1ab
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-23 18:36:54 +0900

    Added useUpdateKarte

---
+++ frontend/src/features/karte/useUpdatekarte.ts
@@ -0,0 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useSetSession as SetSession } from "../../hooks/useSession";
+import { updateDiganosis as updateDiagnosisApi } from "../../services/apiDiagnosis";
+import toast from "react-hot-toast";
+
+export function useUpdateKarte() {
+  const navigate = useNavigate();
+
+  const { mutate: updateKarte, isPending: isUpdating } = useMutation({
+    mutationFn: updateDiagnosisApi,
+    onSuccess: (data) => {
+      SetSession({
+        key: "karte",
+        value: `${JSON.stringify(data.diagnosis)}`,
+      });
+      toast.success("診断結果を更新しました");
+      navigate("/result");
+    },
+    onError: (err) => {
+      toast.error("診断結果の更新に失敗しました");
+      console.error(err.message);
+    },
+  });
+
+  return { updateKarte, isUpdating };
+}
