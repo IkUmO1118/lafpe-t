a8f323a50a9cdeee0f65a0b0bea63855885e6ff1
Ikumo Takahashi <ikumo1118free@gmail.com>
2025-03-24 17:00:01 +0900

    Fixed h-full

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -6,7 +6,7 @@ function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col bg-neutral-50">
       <Header />
-      <main className="flex-1 bg-neutral-50">
+      <main className="h-full flex-1 bg-neutral-50">
         <Outlet />
       </main>
       <Footer />
